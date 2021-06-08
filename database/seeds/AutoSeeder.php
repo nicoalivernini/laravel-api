ea779edad99aa57bdc6b9714e29832a236a40499
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-06-08 15:47:47 +0200

    Creazione Seeder

--- database/seeds/AutoSeeder.php
+++ database/seeds/AutoSeeder.php
@@ -16,8 +16,8 @@ class AutoSeeder extends Seeder
       for ($i=0; $i < 10 ; $i++) {
         $auto = new Auto();
 
-        $auto->model = $faker->randomElements(['Model X', 'Kuga', 'Q7', 'Serie 2', 'Classe E']);
-        $auto->brand = $faker->randomElements(['Tesla', 'Ford', 'Audi', 'BMW', 'Mercedes']);
+        $auto->model = $faker->randomElement(['Model X', 'Kuga', 'Q7', 'Serie 2', 'Classe E']);
+        $auto->brand = $faker->randomElement(['Tesla', 'Ford', 'Audi', 'BMW', 'Mercedes']);
         $auto->doors = $faker->numberBetween(0, 5);
 
         $auto->save();
